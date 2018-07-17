e9b29e454b9d5c39b4bc78b67b8b127b378e1125
Aaron Ang <awz.ang@gmail.com>
2018-07-16 22:31:04 -0700

    Lex arithmetics

---
+++ src/main.rs
@@ -0,0 +1,98 @@
+#[derive(Debug)]
+enum Token {
+    Number(f32),
+    Plus,
+    Minus,
+    Star,
+    Slash,
+}
+
+struct Lexer {
+    source: String,
+    position: usize,
+}
+
+impl Lexer {
+    pub fn from(source: String) -> Lexer {
+        Lexer {
+            source: source,
+            position: 0,
+        }
+    }
+
+    fn skip_whitespace(&mut self) {
+        self.position += self.source[self.position..]
+            .chars()
+            .take_while(|c| c.is_whitespace())
+            .collect::<String>()
+            .len()
+    }
+
+    fn number(&self) -> String {
+        let mut found_dot = false;
+        self.source[self.position..]
+            .chars()
+            .take_while(|n| match n {
+                n if n.is_numeric() => true,
+                '.' if !found_dot => {
+                    found_dot = true;
+                    true
+                }
+                _ => false,
+            })
+            .collect::<String>()
+    }
+
+    fn next_char(&self) -> char {
+        self.source[self.position..].chars().next().unwrap()
+    }
+
+    fn scan(&mut self) -> Option<Token> {
+        if self.position == self.source.len() {
+            return None;
+        }
+
+        self.skip_whitespace();
+
+        match self.next_char() {
+            '0'...'9' => {
+                let lexeme = self.number();
+                self.position += lexeme.len();
+                Some(Token::Number(lexeme.parse().unwrap()))
+            }
+            '+' => {
+                self.position += '+'.len_utf8();
+                Some(Token::Plus)
+            }
+            '-' => {
+                self.position += '-'.len_utf8();
+                Some(Token::Minus)
+            }
+            '*' => {
+                self.position += '*'.len_utf8();
+                Some(Token::Star)
+            }
+            '/' => {
+                self.position += '/'.len_utf8();
+                Some(Token::Slash)
+            }
+            _ => None,
+        }
+    }
+}
+
+impl Iterator for Lexer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Token> {
+        self.scan()
+    }
+}
+
+fn main() {
+    let source = String::from("1 + 2 - 3 * 4 * 5");
+    let lexer = Lexer::from(source);
+    for token in lexer {
+        println!("{:?}", token);
+    }
+}
