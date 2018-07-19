816bb2d5ce72a4254c5d6ed0e75c0bdb7b66b6e6
Aaron Ang <awz.ang@gmail.com>
2018-07-18 22:31:09 -0700

    Allow file as input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 extern crate rustylox;
 use rustylox::scanner::Scanner;
+use std::env;
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    match args.as_slice() {
+        [_] => run_prompt(),
+        [_, path] => run_file(path),
+        _ => println!("Usage: rusty-lox [script]"),
+    }
+}
+
+fn run_file(path: &String) {
+    let mut f = File::open(path).expect("File not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
+    run(&contents);
+}
+
+fn run_prompt() {
     let stdin = io::stdin();
     let mut handle = stdin.lock();
     loop {
@@ -11,13 +31,15 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut input = String::new();
         match handle.read_line(&mut input) {
-            Ok(_) => {
-                let scanner = Scanner::from(&input[..]);
-                for token in scanner {
-                    println!("{:?}", token);
-                }
-            }
+            Ok(_) => run(&input),
             Err(error) => println!("Error: {}", error),
         }
+    }
+}
+
+fn run(source: &String) {
+    let scanner = Scanner::from(&source[..]);
+    for token in scanner {
+        println!("{:?}", token);
     }
 }
